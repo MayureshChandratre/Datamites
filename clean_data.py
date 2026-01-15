3d7e283f8bef7b260b7df7bc68b17c16a58b9d3b
MKC <mayureshchandratre4@gmail.com>
2026-01-15 10:46:57 +0530

    Added two function find and visual

--- clean_data.py
+++ clean_data.py
@@ -1,3 +1,11 @@
 def clean():
   print("We are cleaning Data...");
 print(clean());
+
+def find():
+  print("We are finding data...");
+print(find());
+
+def visual();
+  print("We are preparing visual...");
+print(visual());
